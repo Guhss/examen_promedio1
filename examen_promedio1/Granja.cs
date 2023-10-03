eec86bb1aec1b0cdcc33ecece321ac88d92eedd0
Guhss <abarca_1705@hotmail.com>
2023-10-02 23:10:08 -0500

    Avance de clase Granja y menú de interacción - Anthony Abarca

--- examen_promedio1/Granja.cs
+++ examen_promedio1/Granja.cs
@@ -12,5 +12,83 @@ namespace examen_promedio1
         private List<Planta> plantas;
         private List<Animal> animales;
         private float precioExpansion;
+
+        public Granja(float dineroInicial)
+        {
+            dinero = dineroInicial;
+            plantas = new List<Planta>();
+            animales = new List<Animal>();
+            precioExpansion = 10.0;
+        }
+
+        public void ComprarEspacioEnGranja()
+        {
+            if (dinero >= precioExpansion)
+            {
+                dinero -= precioExpansion;
+                precioExpansion += 10.0;
+                Console.WriteLine("Espacio en la granja comprado con éxito.");
+            }
+            else
+            {
+                Console.WriteLine("No tienes suficiente dinero para comprar espacio en la granja.");
+            }
+        }
+
+        public void ComprarPlanta(Planta planta)
+        {
+            if (dinero >= planta.ValorSemilla)
+            {
+                dinero -= planta.ValorSemilla;
+                plantas.Add(planta);
+                Console.WriteLine($"Has plantado una {planta.Nombre} en tu granja.");
+            }
+            else
+            {
+                Console.WriteLine("No tienes suficiente dinero para comprar la semilla.");
+            }
+        }
+
+        public void ComprarAnimal(Animal animal)
+        {
+            if (dinero >= animal.PrecioAnimal)
+            {
+                dinero -= animal.PrecioAnimal;
+                animales.Add(animal);
+                Console.WriteLine($"Has comprado un {animal.Nombre} para tu granja.");
+            }
+            else
+            {
+                Console.WriteLine("No tienes suficiente dinero para comprar el animal.");
+            }
+        }
+
+
+        public void MostrarEstadoGranja()
+        {
+            Console.WriteLine($"Dinero disponible en la granja: ${dinero}");
+            Console.WriteLine("Estado de la granja:");
+            Console.WriteLine("Plantas:");
+
+            foreach (var planta in plantas)
+            {
+                if (!planta.Cosechada)
+                {
+                    Console.WriteLine($"- {planta.Nombre} (Tiempo de vida: {planta.TiempoDeVida}, Cosechada: No)");
+                }
+            }
+
+            Console.WriteLine("Animales:");
+
+            foreach (var animal in animales)
+            {
+                if (!animal.Recolectado)
+                {
+                    Console.WriteLine($"- {animal.Nombre} (Tiempo de vida: {animal.TiempoDeVida}, Recolectado: No)");
+                }
+            }
+        }
+
+        
     }
 }
--- examen_promedio1/Program.cs
+++ examen_promedio1/Program.cs
@@ -14,6 +14,50 @@ namespace examen_promedio1
             Console.Write("Ingresa la cantidad de dinero inicial en tu granja: $");
             float dineroInicial = float.Parse(Console.ReadLine());
             Granja granja = new Granja(dineroInicial);
+
+            while (true)
+            {
+                Console.WriteLine("-- Menú de acciones --");
+                Console.WriteLine("1. Comprar espacio en la granja");
+                Console.WriteLine("2. Comprar semillas para plantar");
+                Console.WriteLine("3. Comprar animales");
+                Console.WriteLine("4. Pasar de turno");
+                Console.WriteLine("5. Mostrar estado de la granja");
+                Console.WriteLine("6. Vender productos");
+                Console.WriteLine("7. Salir del juego");
+
+                Console.Write("Selecciona una acción: ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        granja.ComprarEspacioEnGranja();
+                        break;
+
+                    case 2:
+
+
+                    case 3:
+
+
+                    case 4:
+
+
+                    case 5:
+
+
+                    case 6:
+
+
+                    case 7:
+                        Console.WriteLine("Gracias por jugar. ¡Hasta la próxima!");
+                        return;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, selecciona una opción válida.");
+                        break;
+                }
+            }
         }
-    }
 }
