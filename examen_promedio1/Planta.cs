36eb69f77138bfeaf7844f8237a15c556c425511
Guhss <abarca_1705@hotmail.com>
2023-10-02 22:31:40 -0500

    Se creo 3 clases y se avanzo la clase planta - Anthony Abarca

---
+++ examen_promedio1/Planta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen_promedio1
+{
+    internal class Planta
+    {
+        public string Nombre; 
+        public int TiempoDeVida;
+        public int CantidadFrutosVerduras;
+        public float ValorSemilla;
+        public float ValorProducto; 
+        public bool Cosechada;
+
+        public Planta(string nombre, int tiempoDeVida, int cantidadFrutosVerduras, float valorSemilla, float valorProducto)
+        {
+            Nombre = nombre;
+            TiempoDeVida = tiempoDeVida;
+            CantidadFrutosVerduras = cantidadFrutosVerduras;
+            ValorSemilla = valorSemilla;
+            ValorProducto = valorProducto;
+            Cosechada = false;
+        }
+    }
+}
